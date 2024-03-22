e7f26f6cb518d5dd473dd2c9860d678cde671062
Shazam72 <jfrancoiszouba72@gmail.com>
2024-03-22 15:50:01 +0100

    Update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,6 +5,6 @@ sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 sudo chown ubuntu:ubuntu /data -R
 echo "Hello World!" > /data/web_static/releases/test/index.html
 ln -fs /data/web_static/releases/test /data/web_static/current
-sed -i '/\tlocation \//i\\tlocation \/hbnb_static {\n\t\talias /data/web_static/current;\n\t\tautoindex off;\n\t}'  /etc/nginx/sites-available/default
+sed -i '/^\tlocation \//i\\tlocation \/hbnb_static {\n\t\talias /data/web_static/current;\n\t\tautoindex off;\n\t}'  /etc/nginx/sites-available/default
 sudo service nginx restart
 exit
