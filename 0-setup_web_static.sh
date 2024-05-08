99bee5e0e5c039de07442a2877727a6f0c3baec4
Shazam72 <jfrancoiszouba72@gmail.com>
2024-05-08 16:17:06 +0200

    Updating 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,8 +3,7 @@
 sudo apt-get update && sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 sudo chown ubuntu:ubuntu /data -R
-echo "Hello World!" > /data/web_static/releases/test/index.html
-rm -f /data/web_static/current && ln -fs /data/web_static/releases/test /data/web_static/current
-sudo grep -q "/hbnb_static"  /etc/nginx/sites-available/default || sudo sed -i '/^\tlocation \//i\\tlocation \/hbnb_static {\n\t\talias /data/web_static/current;\n\t\tautoindex off;\n\t}'  /etc/nginx/sites-available/default
+echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" > /data/web_static/releases/test/index.html
+rm -f /data/web_static/current && ln -s /data/web_static/releases/test /data/web_static/current
+sudo grep -q "/hbnb_static"  /etc/nginx/sites-available/default || sudo sed -i '/^\tlocation \//i\\tlocation \/hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}'  /etc/nginx/sites-available/default
 sudo service nginx restart
-exit
