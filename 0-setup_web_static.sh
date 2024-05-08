260812f407079628fd03919d6e0b5723f78d16f1
Shazam72 <jfrancoiszouba72@gmail.com>
2024-05-08 17:06:14 +0200

    Updating 0-setup_web_static.sh: rewriting for hbnb_static to /hbnb_static/

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,5 +5,5 @@ sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 sudo chown ubuntu:ubuntu /data -R
 echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" > /data/web_static/releases/test/index.html
 rm -f /data/web_static/current && ln -s /data/web_static/releases/test /data/web_static/current
-sudo grep -q "/hbnb_static"  /etc/nginx/sites-available/default || sudo sed -i '/^\tlocation \//i\\tlocation \/hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}'  /etc/nginx/sites-available/default
+sudo grep -q "/hbnb_static"  /etc/nginx/sites-available/default || sudo sed -i '/^\tlocation \//i\\tlocation \/hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}'  /etc/nginx/sites-available/default && sudo sed -i '/\tlocation \/hbnb_static /i\\tlocation = /hbnb_static {\n\t\trewrite ^ /hbnb_static/ last;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
