28ea4d3b8fa51e7004f1e247fd6d32f82e87b18a
Shazam72 <jfrancoiszouba72@gmail.com>
2024-05-10 16:58:56 +0200

    Giving ownership of everything too ubuntu user

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,9 +2,9 @@
 # sets up web servers for deployment of web_static
 sudo apt-get update && sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-sudo chown ubuntu:ubuntu /data -R
 echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" > /data/web_static/releases/test/index.html
 rm -f /data/web_static/current && ln -s /data/web_static/releases/test /data/web_static/current
+sudo chown ubuntu:ubuntu /data -R
 sudo grep -q "/hbnb_static"  /etc/nginx/sites-available/default || sudo sed -i '/^\tlocation \//i\\tlocation \/hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}'  /etc/nginx/sites-available/default
 sudo grep -q "location = /hbnb_static" /etc/nginx/sites-available/default || sudo sed -i '/\tlocation \/hbnb_static /i\\tlocation = /hbnb_static {\n\t\trewrite ^ /hbnb_static/ last;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
